6645181e483af055cfc6c3c99039ad49c1e37538
lesly2992 <91107710+lesly2992@users.noreply.github.com>
2024-11-23 15:35:06 -0500

    Delete Agencia_de_Viajes directory

--- Agencia_de_Viajes/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <div class="container">
-        <p>&copy; 2024 Agencia de Viajes. Todos los derechos reservados.</p>
-    </div>
-</footer>
-</body>
-</html>
--- Agencia_de_Viajes/includes/nav.php
+++
@@ -1,9 +0,0 @@
-<nav class= "global-nav">
-    <div class="container">
-        <a href="/Agencia_de_Viajes/index.php">Inicio</a>
-        <a href="/pages/packages.php">Paquetes</a>
-        <a href="/pages/about.php">Sobre Nosotros</a>
-        <a href="/pages/contact.php">Contacto</a>
-        <a href="/Agencia_de_Viajes/pages/login.php">Iniciar Sesión</a>
-    </div>
-</nav>
